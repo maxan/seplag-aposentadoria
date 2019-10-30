fb806625021d499e7d1559789f71e7296afd8671
Anderson Marinho <anmdss@msn.com>
2019-10-30 18:15:39 -0300

    Inclusão do backend. Frontend movido para subpasta separada.

---
+++ backend/src/main/java/br/gov/ceara/aposentadoria/AposentadoriaApplication.java
@@ -0,0 +1,13 @@
+package br.gov.ceara.aposentadoria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AposentadoriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AposentadoriaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/gov/ceara/aposentadoria/AposentadoriaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.ceara.aposentadoria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AposentadoriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
